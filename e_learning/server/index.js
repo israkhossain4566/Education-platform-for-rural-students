e5c4cee37549a0b95aa0c51935921459cb3f7b20
Aumio Rahman <aumiorahman6@gmail.com>
2024-07-11 21:59:06 +0600

    backend and otp verification

---
+++ e_learning/server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// using middlewares
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+
+// importing routes
+import userRoutes from './routes/user.js';
+
+
+// using routes
+app.use('/api', userRoutes);
+
+app.listen(5000, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    connectDb()
+});
---
+++ e_learning/server/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+    {
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        Unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        default: "user",
+
+    },
+    subsciption:[
+                {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Course",
+            },
+    ],
+    },{
+        timestamps: true,
+    }
+
+);
+
+export const User = mongoose.model("User", schema);
